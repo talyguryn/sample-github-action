13cda4129b3dd4af58fb777ae94b7b769ffd88d4
Taly <vitalik7tv@yandex.ru>
2020-11-11 15:10:17 +0300

    codex_bot notify

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+import querystring from 'querystring';
+import axios from 'axios';
+
+/**
+ * Message formatting styles
+ * @type {string[]}
+ */
+const PARSE_MODE_STYLES = [
+    'HTML',
+    'Markdown',
+    'MarkdownV2'
+]
+
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
+    /**
+     * Webhook url
+     * @type {string}
+     */
+    const webhook = core.getInput('webhook');
+    if (!webhook) {
+        throw new Error('`webhook` param is missing');
+    }
 
-    console.log(`Hello ${nameToGreet}!`);
+    /**
+     * Message to send
+     * @type {string}
+     */
+    const message = core.getInput('message');
+    if (!message) {
+        throw new Error('`message` param is missing');
+    }
 
-    const time = (new Date()).toTimeString();
+    /**
+     * Message formatting style
+     * @type {string}
+     */
+    const parse_mode = core.getInput('parse_mode');
+    if (parse_mode && !PARSE_MODE_STYLES.includes(parse_mode)) {
+        throw new Error(`Bad \`parse_mode\` param. Use one of the following: ${PARSE_MODE_STYLES.join(', ')}`);
+    }
 
-    core.setOutput("time", time);
+    const disable_web_page_preview = core.getInput('disable_web_page_preview');
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-
-    console.log(`The event payload: ${payload}`);
+    axios({
+        method: 'POST',
+        url: webhook,
+        data: querystring.stringify({
+            message: message,
+            parse_mode: parse_mode,
+            disable_web_page_preview: !!disable_web_page_preview,
+        })
+    })
+        .then(response => {
+            core.setOutput("response-body", response.data);
+            core.setOutput("response-code", response.status);
+        })
+        .catch(error => {
+            core.setFailed(error.message);
+        });
 } catch (error) {
     core.setFailed(error.message);
 }
